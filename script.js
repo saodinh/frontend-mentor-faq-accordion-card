de350ff56e79d091394b1b5567b2bf354bafe664
saodinh <saodinh96@gmail.com>
2022-01-04 07:06:20 +0700

    Implement not-always-open dropdown

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// const dropdowns = document.querySelectorAll(".accordion__dropdown");
+const dropdowns = document.querySelectorAll(".accordion__dropdown");
 const dropdownButtons = document.querySelectorAll(".dropdown__title");
 
 dropdownButtons.forEach((dropdownButton) =>
     dropdownButton.addEventListener("click", () => {
-        dropdownButton.parentNode.classList.toggle("active");
+        if (dropdownButton.parentNode.classList.contains("active"))
+            dropdownButton.parentNode.classList.remove("active");
+        else {
+            // Remove active state from other currently active dropdown. Remove this line if wanting to always open dropdown.
+            dropdowns.forEach((dropdown) => dropdown.classList.remove("active"));
+            dropdownButton.parentNode.classList.add("active");
+        }
     })
 );
